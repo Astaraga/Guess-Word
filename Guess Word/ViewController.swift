8342ac5309a2b8a580fb33e172aca2b66122ab14
Tapan Raut <rinku.tapan.raut@gmail.com>
2019-07-03 01:44:30 +0530

    some variable, array and functions are created

--- Guess Word/ViewController.swift
+++ Guess Word/ViewController.swift
@@ -17,6 +17,31 @@ class ViewController: UIViewController,UITextFieldDelegate {
     
     @IBOutlet weak var letterBankLabel: UILabel!
     
+    // Create Array to hold hints and letters(need word to guess and word as under score variable)
+    
+    let LIST_OF_WORDS: [String] = []
+    let LIST_OF_HINTS: [String] = []
+    var wordToGuess: String!
+    var wordUnderScores: String!
+    let MAX_NUMBER_OF_GUESSES: Int = 5
+    var guessRemaining: Int!
+    
+    // need some way randomize which word is being selected, then select theword and hint and display them
+    
+    //need way to keep track of number to guesses being used
+    
+    //  need way to guess letters and process wheather or not it is in the wordToGuess. If correct, display updated words as underscores . if incorrect decrement the number of guesses, add letters letter to letter bank and display letters guessed
+    
+    //lose game if runout of guesses
+    
+    //win game if guess every letters before running out of guesses(if words as underscscores contains no more underscores, the we win)
+
+    
+    
+    
+    
+    
+    
     
     
     override func viewDidLoad() {
@@ -36,5 +61,17 @@ class ViewController: UIViewController,UITextFieldDelegate {
         
     }
 
+    @IBAction func chooseNewWordAction(_ sender: UIButton) {
+    }
+    
+    func chooseRandomNumbers() {
+        
+    }
+    func processCorrectGuess() {
+        
+    }
+    func processInCorrectGuess() {
+        
+    }
 }
 
